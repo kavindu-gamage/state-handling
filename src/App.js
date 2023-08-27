b69086d3c44432838b12589f42fd4c761b1a5694
Kavindu <kavindu2308@gmail.com>
2023-08-27 18:31:07 +0530

    State Handling Practice Code

--- src/App.js
+++ src/App.js
@@ -1,23 +1,47 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from 'react';
+import './App.scss';
 
 function App() {
+  const [counter, setCounter] = useState(0);
+  const [firstname, setFirstName] = useState("");
+  const [showWelcome, setShowWelcome] =useState(false);
+
+  const increaseCounter=()=>(
+    setCounter(counter+1)
+  )
+
+  const decreaseCounter=()=>(
+    setCounter(counter-1)
+  )
+    
+  const handleInput = (event) => (
+  setFirstName (event.target.value)
+  )
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    setShowWelcome(true);
+  }
+
+  useEffect(()=> {
+    document.title = "Greetings to " + firstname
+  });
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Counter: {counter}</h1>
+      <button type='button' onClick={increaseCounter}>Increase</button>
+      <button type='button' onClick={decreaseCounter}>Decrease</button>
+
+      {showWelcome &&
+        <h1>Hello {firstname}!</h1>
+      }
+      
+
+      <form onSubmit={handleSubmit} >
+        <input  type="text" onChange={handleInput} />
+        <button type='submit' >Show</button>
+      </form>
     </div>
   );
 }
